a59ab60c1461684d81a8df5039e4487eec83129a
Kuangye Chen <chen.kuangye@gmail.com>
2019-03-19 22:43:01 +0100

    update

--- zsh-rm2trash.plugin.zsh
+++ zsh-rm2trash.plugin.zsh
@@ -15,7 +15,7 @@ function _rm_to_trash() {
     if ! [[ $arg == -* ]]; then
       mv $arg $ZSH_RM2TRASH_TRASH_DIR/${arg##*/}_`date +%Y-%m-%d_%H-%M-%S`
       if [[ $? == 0 ]]; then
-        echo "mv $arg to $ZSH_RM2TRASH_TRASH_DIR with timestamp."
+        echo -e "mv $arg to $ZSH_RM2TRASH_TRASH_DIR with timestamp."
       fi
     fi
   done
@@ -31,19 +31,21 @@ function _clear_trash() {
   _check_trash_dir
 
   echo -e "Are you sure to delete all files in $ZSH_RM2TRASH_TRASH_DIR? [y/n]"
-  echo -e "\033[91m\033[1mWARN: YOU CAN NOT UNDO THIS.\033[0m\033[0m"
+  echo -e "\033[91m\033[1mWARN: YOU CANNOT UNDO THIS.\033[0m\033[0m"
 
   declare ans
   read ans
 
   if [[ $ans =~ ^[Yy]{1}$ ]]; then
+    echo -e "Permanently deleting all files in $ZSH_RM2TRASH_TRASH_DIR."
     /bin/rm -vrf $ZSH_RM2TRASH_TRASH_DIR/*
   else
-    echo "Do nothing."
+    echo -e "Do nothing."
   fi
 } 
 
-ZSH_RM2TRASH_TRASH_DIR=$HOME/.trash
+ZSH_RM2TRASH_TRASH_DIR=$HOME/.Trash
+
 alias rm='_rm_to_trash'
 alias rm2trash='_rm_to_trash'
 alias lstrash='_list_trash'
