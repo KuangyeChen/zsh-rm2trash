c27339c7ee1a6a85f7422b504d0c2bffbbc2d015
Kuangye Chen <chen.kuangye@gmail.com>
2019-08-01 01:30:10 +0200

    update

--- zsh-rm2trash.plugin.zsh
+++ zsh-rm2trash.plugin.zsh
@@ -8,19 +8,19 @@ function _check_trash_dir() {
 function _rm_to_trash() {
   _check_trash_dir
 
-  for arg in $@; do
-    if [[ $arg == -* ]]; then continue; fi
+  for arg in ${@}; do
+    if [[ ${arg} == -* ]]; then continue; fi
     
-    if [[ -L $arg ]]; then
-      message="Remove softlink $arg."
-      /bin/rm $arg
+    if [[ -L ${arg} ]]; then
+      message="Remove softlink ${arg}."
+      /bin/rm ${arg}
     else
-      message="mv $arg to $ZSH_RM2TRASH_TRASH_DIR with timestamp."
-      mv $arg $ZSH_RM2TRASH_TRASH_DIR/${arg##*/}_`date +%Y-%m-%d_%H-%M-%S`
+      message="mv ${arg} to ${ZSH_RM2TRASH_TRASH_DIR} with timestamp."
+      mv ${arg} ${ZSH_RM2TRASH_TRASH_DIR}/${arg##*/}_`date +%Y-%m-%d_%H-%M-%S`
     fi
   
-    if [[ $? == 0 ]]; then
-      echo -e $message
+    if [[ ${?} == 0 ]]; then
+      echo -e ${message}
     fi
   done
 }
@@ -34,13 +34,12 @@ function _list_trash() {
 function _clear_trash() {
   _check_trash_dir
 
-  echo -e "Are you sure to delete all files in ${ZSH_RM2TRASH_TRASH_DIR}? [y/n]"
   echo -e "\033[91m\033[1mWARN: YOU CANNOT UNDO THIS.\033[0m\033[0m"
+  echo -e "Are you sure to delete all files in ${ZSH_RM2TRASH_TRASH_DIR}? [y/n]"
 
-  declare ans
   read ans
 
-  if [[ $ans =~ ^[Yy]{1}$ ]]; then
+  if [[ ${ans} =~ ^[Yy]{1}$ ]]; then
     echo -e "Permanently deleting all files in ${ZSH_RM2TRASH_TRASH_DIR}."
     /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/*
   else
