be024ffe90f7820790e32a0c0fdc08c949b76088
Kuangye Chen <chen.kuangye@gmail.com>
2019-08-01 15:11:09 +0200

    update

--- zsh-rm2trash.plugin.zsh
+++ zsh-rm2trash.plugin.zsh
@@ -1,12 +1,12 @@
-function _check_trash_dir() {
+function _zsh_rm2trash_check_trash_dir() {
   if ! [[ -d ${ZSH_RM2TRASH_TRASH_DIR} ]]; then
     echo -e "\033[1mWARN:\033[0m ${ZSH_RM2TRASH_TRASH_DIR} not exists. Create trash directory ${ZSH_RM2TRASH_TRASH_DIR}."
     mkdir -p ${ZSH_RM2TRASH_TRASH_DIR}
   fi
 }
 
-function _rm_to_trash() {
-  _check_trash_dir
+function _zsh_rm2trash_rm_to_trash() {
+  _zsh_rm2trash_check_trash_dir
 
   for arg in ${@}; do
     if [[ ${arg} == -* ]]; then continue; fi
@@ -25,14 +25,14 @@ function _rm_to_trash() {
   done
 }
 
-function _list_trash() {
-  _check_trash_dir
+function _zsh_rm2trash_list_trash() {
+  _zsh_rm2trash_check_trash_dir
 
   ls -ahl ${ZSH_RM2TRASH_TRASH_DIR}
 }
 
-function _clear_trash() {
-  _check_trash_dir
+function _zsh_rm2trash_clear_trash() {
+  _zsh_rm2trash_check_trash_dir
 
   echo -e "\033[91m\033[1mWARN: YOU CANNOT UNDO THIS.\033[0m\033[0m"
   echo -e "Are you sure to delete all files in ${ZSH_RM2TRASH_TRASH_DIR}? [y/n]"
@@ -41,22 +41,22 @@ function _clear_trash() {
 
   if [[ ${ans} =~ ^[Yy]{1}$ ]]; then
     echo -e "Permanently deleting all files in ${ZSH_RM2TRASH_TRASH_DIR}."
-    /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/*
+    /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/* || true; /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/.* || true 
   else
     echo -e "Do nothing."
   fi
 } 
 
-function _cd_to_trash() {
-  _check_trash_dir
+function _zsh_rm2trash_cd_to_trash() {
+  _zsh_rm2trash_check_trash_dir
 
   cd ${ZSH_RM2TRASH_TRASH_DIR}
 }
 
 export ZSH_RM2TRASH_TRASH_DIR=${HOME}/.Trash
 
-alias rm='_rm_to_trash'
-alias rm2trash='_rm_to_trash'
-alias lstrash='_list_trash'
-alias cleartrash='_clear_trash'
-alias cdtrash='_cd_to_trash'
+alias rm='_zsh_rm2trash_rm_to_trash'
+alias rm2trash='_zsh_rm2trash_rm_to_trash'
+alias lstrash='_zsh_rm2trash_list_trash'
+alias cleartrash='_zsh_rm2trash_clear_trash'
+alias cdtrash='_zsh_rm2trash_cd_to_trash'
