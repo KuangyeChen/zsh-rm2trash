c3889b6873844d4df6cfc2c5e43ce40415b9f01a
Kuangye Chen <chen.kuangye@gmail.com>
2019-08-02 20:16:26 +0200

    update

--- zsh-rm2trash.plugin.zsh
+++ zsh-rm2trash.plugin.zsh
@@ -1,59 +1,59 @@
 function _zsh_rm2trash_check_trash_dir() {
-  if ! [[ -d ${ZSH_RM2TRASH_TRASH_DIR} ]]; then
-    echo -e "\033[1mWARN:\033[0m ${ZSH_RM2TRASH_TRASH_DIR} not exists. Create trash directory ${ZSH_RM2TRASH_TRASH_DIR}."
-    mkdir -p ${ZSH_RM2TRASH_TRASH_DIR}
-  fi
+    if ! [[ -d ${ZSH_RM2TRASH_TRASH_DIR} ]]; then
+        echo -e "\033[1mWARN:\033[0m ${ZSH_RM2TRASH_TRASH_DIR} not exists. Create trash directory ${ZSH_RM2TRASH_TRASH_DIR}."
+        mkdir -p ${ZSH_RM2TRASH_TRASH_DIR}
+    fi
 }
 
 function _zsh_rm2trash_rm_to_trash() {
-  _zsh_rm2trash_check_trash_dir
+    _zsh_rm2trash_check_trash_dir
 
-  local arg
-  local message
-  for arg in ${@}; do
-    if [[ ${arg} == -* ]]; then continue; fi
-    
-    if [[ -L ${arg} ]]; then
-      message="Remove softlink ${arg}."
-      /bin/rm ${arg}
-    else
-      message="mv ${arg} to ${ZSH_RM2TRASH_TRASH_DIR} with timestamp."
-      mv ${arg} ${ZSH_RM2TRASH_TRASH_DIR}/${arg##*/}_`date +%Y-%m-%d_%H-%M-%S`
-    fi
+    local arg
+    local message
+    for arg in ${@}; do
+        if [[ ${arg} == -* ]]; then continue; fi
+
+        if [[ -L ${arg} ]]; then
+            message="Remove softlink ${arg}."
+            /bin/rm ${arg}
+        else
+            message="mv ${arg} to ${ZSH_RM2TRASH_TRASH_DIR} with timestamp."
+            mv ${arg} ${ZSH_RM2TRASH_TRASH_DIR}/${arg##*/}_`date +%Y-%m-%d_%H-%M-%S`
+        fi
   
-    if [[ ${?} == 0 ]]; then
-      echo -e ${message}
-    fi
-  done
+        if [[ ${?} == 0 ]]; then
+            echo -e ${message}
+        fi
+    done
 }
 
 function _zsh_rm2trash_list_trash() {
-  _zsh_rm2trash_check_trash_dir
+    _zsh_rm2trash_check_trash_dir
 
-  ls -ahl ${ZSH_RM2TRASH_TRASH_DIR}
+    ls -ahl ${ZSH_RM2TRASH_TRASH_DIR}
 }
 
 function _zsh_rm2trash_clear_trash() {
-  _zsh_rm2trash_check_trash_dir
+    _zsh_rm2trash_check_trash_dir
 
-  echo -e "\033[91m\033[1mWARN: YOU CANNOT UNDO THIS.\033[0m\033[0m"
-  echo -e "Are you sure to delete all files in ${ZSH_RM2TRASH_TRASH_DIR}? [y/n]"
+    echo -e "\033[91m\033[1mWARN: YOU CANNOT UNDO THIS.\033[0m\033[0m"
+    echo -e "Are you sure to delete all files in ${ZSH_RM2TRASH_TRASH_DIR}? [y/n]"
 
-  local ans
-  read ans
+    local ans
+    read ans
 
-  if [[ ${ans} =~ ^[Yy]{1}$ ]]; then
-    echo -e "Permanently deleting all files in ${ZSH_RM2TRASH_TRASH_DIR}."
-    /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/* || true; /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/.* || true 
-  else
-    echo -e "Do nothing."
-  fi
+    if [[ ${ans} =~ ^[Yy]{1}$ ]]; then
+        echo -e "Permanently deleting all files in ${ZSH_RM2TRASH_TRASH_DIR}."
+        /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/* || true; /bin/rm -vrf ${ZSH_RM2TRASH_TRASH_DIR}/.* || true 
+    else
+        echo -e "Do nothing."
+    fi
 } 
 
 function _zsh_rm2trash_cd_to_trash() {
-  _zsh_rm2trash_check_trash_dir
+    _zsh_rm2trash_check_trash_dir
 
-  cd ${ZSH_RM2TRASH_TRASH_DIR}
+    cd ${ZSH_RM2TRASH_TRASH_DIR}
 }
 
 export ZSH_RM2TRASH_TRASH_DIR=${HOME}/.Trash
